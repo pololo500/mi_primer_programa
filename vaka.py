d1314b7be9e6d1d4d44e70b2e922f2bfd9c41e68
tomas <pololo5007@gmail.com>
2019-05-15 20:24:23 -0300

    adivina el numero

---
+++ vaka.py
@@ -0,0 +1,28 @@
+
+number_to_guess = 2
+numero_de_usuario = int(input("adivina el numero"))
+if number_to_guess == numero_de_usuario:
+    print("has ganado")
+    none
+else:
+    numero_de_usuario = int(input("adivina el numero otra vez"))
+if number_to_guess == numero_de_usuario:
+    print("has ganado")
+
+else:
+    numero_de_usuario = int(input("adivina el numero otra vez"))
+if number_to_guess == numero_de_usuario:
+    print("has ganado")
+
+else:
+    numero_de_usuario = int(input("adivina el numero otra vez"))
+if number_to_guess == numero_de_usuario:
+    print("has ganado")
+
+else:
+    numero_de_usuario = int(input("adivina el numero otra vez"))
+if number_to_guess == numero_de_usuario:
+    print("has ganado")
+
+else:
+    print("tuviste 5 oportunidades puta")
